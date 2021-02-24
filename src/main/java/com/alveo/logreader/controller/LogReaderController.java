fb75fdb2741df6827f783531ff1cbcb832144c1f
Abhishek <abhishek.gaonkar07@gmail.com>
2021-02-24 19:50:46 +0100

    Updated Swagger and ui code

--- src/main/java/com/alveo/logreader/controller/LogReaderController.java
+++ src/main/java/com/alveo/logreader/controller/LogReaderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Controller class for log file read.
@@ -22,8 +23,8 @@ public class LogReaderController {
     @Value("${monitoring.interval}")
     private Integer monitoringInterval;
 
-    @GetMapping(value = "/log/count/{fileName}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, Integer> getLogLevelCount(@PathVariable(required = true, name = "fileName") final String fileName, @PathVariable(required = false, name = "intervalInSeconds") final Integer intervalInSeconds){
-        return logReaderService.countLogLevelLogs(fileName, intervalInSeconds == null ? monitoringInterval : intervalInSeconds);
+    @GetMapping(value = {"/log/count/{fileName}" , "/log/count/{fileName}/{intervalInSeconds}"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Integer> getLogLevelCount(@PathVariable(required = true, name = "fileName") final String fileName, @PathVariable(required = false, name = "intervalInSeconds") final Optional<Integer> intervalInSeconds){
+        return logReaderService.countLogLevelLogs(fileName, intervalInSeconds.isPresent() ? intervalInSeconds.get() : monitoringInterval);
     }
 }
